7f86178ce33be66e1d410e03a915629298344fe6
Matheus Gomes <eu@matheusgomes.com.br>
2015-06-22 17:56:47 -0300

    VoidTransaction method

---
+++ src/Method/VoidTransaction.php
@@ -0,0 +1,38 @@
+<?php
+namespace EuMatheusGomes\Komerci\Method;
+
+use EuMatheusGomes\Komerci\Method\AbstractMethod;
+
+class VoidTransaction extends AbstractMethod
+{
+    protected $options = [
+        'Total' => '',
+        'Filiacao' => '',
+        'NumCV' => '',
+        'NumAutor' => '',
+        'Concentrador' => '',
+        'Usr' => '',
+        'Pwd' => ''
+    ];
+
+    public function __construct(\EuMatheusGomes\Komerci\KomerciClient $komerciClient)
+    {
+        parent::__construct($komerciClient);
+
+        $this->method = 'VoidTransaction';
+        $this->resultNodeName = 'VoidTransactionResult';
+
+        $this->options['Filiacao'] = $komerciClient->getFiliacao();
+        $this->options['Usr'] = $komerciClient->getUsr();
+        $this->options['Pwd'] = $komerciClient->getPwd();
+    }
+
+    public function approved($response)
+    {
+        if ((int) $resposta->codret === 0) {
+            return true;
+        }
+
+        return false;
+    }
+}
