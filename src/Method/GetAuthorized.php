02be54dd70366b3f3d6de0cd865b562718218741
Matheus Gomes <eu@matheusgomes.com.br>
2015-06-22 17:52:16 -0300

    GetAuthorized method

---
+++ src/Method/GetAuthorized.php
@@ -0,0 +1,54 @@
+<?php
+namespace EuMatheusGomes\Komerci\Method;
+
+use EuMatheusGomes\Komerci\Method\AbstractMethod;
+
+class GetAuthorized extends AbstractMethod
+{
+    protected $options = [
+        'Total'        => '',
+        'Transacao'    => '',
+        'Parcelas'     => '',
+        'Filiacao'     => '',
+        'NumPedido'    => '',
+        'Nrcartao'     => '',
+        'CVC2'         => '',
+        'Mes'          => '',
+        'Ano'          => '',
+        'Portador'     => '',
+        'ConfTxn'      => '',
+        'IATA'         => '',
+        'Distribuidor' => '',
+        'Concentrador' => '',
+        'TaxaEmbarque' => '',
+        'Entrada'      => '',
+        'Pax1'         => '',
+        'Pax2'         => '',
+        'Pax3'         => '',
+        'Pax4'         => '',
+        'Numdoc1'      => '',
+        'Numdoc2'      => '',
+        'Numdoc3'      => '',
+        'Numdoc4'      => '',
+        'AddData'      => ''
+    ];
+
+    public function __construct(\EuMatheusGomes\Komerci\KomerciClient $komerciClient)
+    {
+        parent::__construct($komerciClient);
+
+        $this->method = 'GetAuthorized';
+        $this->resultNodeName = 'GetAuthorizedResult';
+
+        $this->options['Filiacao'] = $komerciClient->getFiliacao();
+    }
+
+    public function approved($response)
+    {
+        if ((int) $response->CODRET === 0 && (string) trim($response->NUMCV) !== '') {
+            return true;
+        }
+
+        return false;
+    }
+}
